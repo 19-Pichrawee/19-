4a9233ce5a9d628d81a7432a97949c07c77c64f3
19-Pichrawee <pichrawee33@gmail.com>
2026-09-17 15:41:09 +0630

    Update Examination_4.py

--- Examination_4.py
+++ Examination_4.py
@@ -7,4 +7,12 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-weight = float(input())
+weight = float(input("น้ำหนักสินค้า"))
+output = int(input("ค่าจัดส่ง (30, 50, 100)"))
+
+if weight <= 1 kg
+    print "30"
+elif weight <= 5 kg
+    print "50"
+else weight > 5 kg
+    print "100"
